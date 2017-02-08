49e72e43a3a00de8e5871d15e366653c78d94f74
Baran Ozbek <xitenebula@gmail.com>
2017-02-08 13:24:10 +0000

    Added static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello html: "Hello, World!"
-    render
+  def hello 
+    render html: "Hello, World!"
   end
   
 end
